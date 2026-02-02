adf36a21469affd384aafdfcf45b492b25f67564
SHREENIDHI PANI <143885806+shreenidhipani2001@users.noreply.github.com>
2026-02-02 22:19:53 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,32 +1,9 @@
-// // src/lib/auth.ts
-// import { apiFetch } from "./api";
-
-// export async function login(email: string, password: string) {
-//   console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
-//   console.log("Logging in with:", { email, password });
-//   console.log(`${process.env.NEXT_PUBLIC_API_URL}/api/users/login`)
-//   return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/login`, {
-//     method: "POST",
-//     credentials: "include", // important for cookies
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ email, password }),
-//   }).then(res => res.json());
-// }
-
-// export async function refreshToken() {
-//   return apiFetch("/auth/refresh", {
-//     method: "POST",
-//   });
-// }
-
-// export async function logout() {
-//   return apiFetch("/auth/logout", {
-//     method: "POST",
-//   });
-// }
+ 
 
 import { apiFetch } from "./api";
-import { useAuthStore } from "../../ecoDarsiniFrontend/src/store/useAuthStore";
+import { useAuthStore } from "../src/store/useAuthStore";
+
+//import { useAuthStore } from "../../ecoDarsiniFrontend/src/store/useAuthStore";
 // import { useAu thStore } from "../store/useAuthStore";
 
 /* ================= LOGIN ================= */
