c87ba3c4add7d8b280a6542efb5c1d36caf999c2
Shreenidhi Pani <shreenidhipani@MacBook-Air.local>
2026-02-05 01:16:15 +0530

    dd

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'localhost',
+          port: '3001',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'ecodarshinibackend-1.onrender.com',
+          pathname: '/**',
+        },
+      ],
+    },
+  };
+
+module.exports = nextConfig;
