ba570983c503c04ade424493e2bd6a19ee97fd6e
SHREENIDHI PANI <143885806+shreenidhipani2001@users.noreply.github.com>
2026-02-04 23:26:16 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ecodarshinibackend-1.onrender.com',
+        pathname: '/**',
+      },
     ],
   },
 };
